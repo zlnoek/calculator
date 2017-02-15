e4f24c9c563349e6799db788ac7af69efafd665c
zlnoek <evangelion_unit-01@hotmail.com>
2017-02-14 21:07:45 -0800

    add event handlers and dynamically generated text

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 //calculator
+
+
+console.log('All TD', $('td'));
+
+$('td').each((ind, el) => {
+  $(el).text($(el).attr('id'));
+});
+
+$('td').on('click', (e) => {
+  let op = $(e.target).text();
+  console.log('Clicked:', op);
+});
+
