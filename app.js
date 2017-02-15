7955ce03a85d6a3c4a1c5c32dd971ef596346ce4
zlnoek <evangelion_unit-01@hotmail.com>
2017-02-14 22:12:15 -0800

    Add calculater handlers and implemented function to update display

--- app.js
+++ app.js
@@ -1,8 +1,53 @@
-//calculator
+// calculator display string
+let display = '0';
 
+// Calculator logic
+const updateDisplay = disp => {
+  $('#display').text(disp);
+};
 
-console.log('All TD', $('td'));
+const checkMD = op => {
+  if (op === '*' || op === '/') {
+    return true;
+  } else {
+    return false;
+  }
+};
 
+const calculate = eq => {
+  console.log('Input Eq:', eq);
+  let cmd = eq.split(' ');
+  cmd.reduce((acc, int, ind, col) => {
+    if (checkMD(int)) {
+
+    }
+  }, []);
+
+};
+
+const calculatorHandler = op => {
+  if (+op === +op) {
+    if (display === '0') {
+      display = op;
+    } else {
+      display += op;
+    }
+    updateDisplay(display);
+  } else if (op === 'c') {
+    display = '0';
+    updateDisplay(display);
+  } else if (op === '=') {
+    calculate(display);
+  } else {
+    if (display !== '0') {
+      let cmd = ` ${op} `;
+      display += cmd;
+      updateDisplay(display);
+    }
+  }
+};
+
+// Add text to buttons based on id
 $('td').each((ind, el) => {
   let $el = $('<div><div/>');
   $el.addClass('calcButton');
@@ -11,11 +56,14 @@ $('td').each((ind, el) => {
   $(el).addClass('buttonContainer');
 });
 
+// Add onClick listener for calculator buttons
 $('td').on('click', (e) => {
   let op = $(e.target).text();
   console.log('Clicked:', op);
+  calculatorHandler(op);
 });
 
+// Add highlighting event handlers
 $('.buttonContainer').on('mouseenter', (e) => {
   let $el = $(e.target);
 
@@ -28,5 +76,6 @@ $('.buttonContainer').on('mouseenter', (e) => {
     $el.removeClass('highlighted');
   });
 });
+
 
 
