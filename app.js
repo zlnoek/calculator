218d3467cfd19f4dcc495708b1f40cb8070724f6
zlnoek <evangelion_unit-01@hotmail.com>
2017-02-14 21:40:54 -0800

    add highlighting css/jquery

--- app.js
+++ app.js
@@ -4,11 +4,29 @@
 console.log('All TD', $('td'));
 
 $('td').each((ind, el) => {
-  $(el).text($(el).attr('id'));
+  let $el = $('<div><div/>');
+  $el.addClass('calcButton');
+  $el.text($(el).attr('id'));
+  $(el).append($el);
+  $(el).addClass('buttonContainer');
 });
 
 $('td').on('click', (e) => {
   let op = $(e.target).text();
   console.log('Clicked:', op);
 });
+
+$('.buttonContainer').on('mouseenter', (e) => {
+  let $el = $(e.target);
+
+  $('.highlighted').removeClass('highlighted');
+
+  $el.addClass('highlighted');
+
+  $('.highlighted').on('mouseleave', (e) => {
+    let $el = $(e.target);
+    $el.removeClass('highlighted');
+  });
+});
+
 
